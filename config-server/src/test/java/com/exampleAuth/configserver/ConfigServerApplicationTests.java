beadeb1a382464d74fc63e5dbdde6f168478da32
Prashant Chavan <prashantch451@gmail.com>
2025-02-25 00:09:32 +0530

    stable base application

---
+++ config-server/src/test/java/com/exampleAuth/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleAuth.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
